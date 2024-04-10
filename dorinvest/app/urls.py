36d1f21d686a9724a6ec850825903774abc63566
ShavnicynK <shavnicyn.k@yandex.ru>
2024-04-10 21:00:30 +0300

    some change

--- dorinvest/app/urls.py
+++ dorinvest/app/urls.py
@@ -3,6 +3,6 @@ from .views import *
 from rest_framework import routers
 
 router = routers.SimpleRouter()
-router.register(r'partners', PartnerViewSet)
+router.register(r'api/expositions/<int:pk>/partners', PartnerViewSet)
 urlpatterns = router.urls
 
